f3c340271ab404ca3301435e82ddec5cbc52cc2a
Kovox91 <sascha.maschmann@hotmail.de>
2025-06-23 10:17:46 +0200

    running prototype

--- setup_env.sh
+++ setup_env.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
-# uv env atomicaenv --python=3.9
-uv pip install numpy==1.26.4
-uv pip install torch==2.1.1+cu118 --extra-index-url https://download.pytorch.org/whl/cu118
-uv pip install torch_scatter torch_cluster --find-links https://pytorch-geometric.com/whl/torch-2.1.1+cu118.html
-uv pip install tensorboard==2.18.0
-uv pip install e3nn==0.5.1 # possibly not compatible with e3nn > 0.5.4
-uv pip install scipy==1.13.1
-uv pip install rdkit-pypi==2022.9.5
-uv pip install openbabel-wheel==3.1.1.20
-uv pip install biopython==1.84
-uv pip install biotite==0.40.0
-uv pip install atom3d
-uv pip install wandb==0.18.2
-uv pip install orjson
+
+# ENVNAME=atomicaenv
+# conda create -n $ENVNAME python=3.9 -y
+# source activate $ENVNAME
+conda install numpy==1.26.4 -y
+pip3 install torch==2.1.1+cu118 --extra-index-url https://download.pytorch.org/whl/cu118
+pip3 install torch_scatter torch_cluster --find-links https://pytorch-geometric.com/whl/torch-2.1.1+cu118.html
+pip3 install tensorboard==2.18.0
+pip3 install e3nn==0.5.1 # possibly not compatible with e3nn > 0.5.4
+pip3 install scipy==1.13.1
+pip3 install rdkit-pypi==2022.9.5
+pip3 install openbabel-wheel==3.1.1.20
+pip3 install biopython==1.84
+pip3 install biotite==0.40.0
+pip3 install atom3d
+pip3 install wandb==0.18.2
+pip3 install orjson
 
 # plotting
-uv pip install umap-learn
-uv pip install matplotlib
-uv pip install seaborn
-uv pip install plotly
+pip3 install umap-learn
+pip3 install matplotlib
+pip3 install seaborn
+pip3 install plotly
